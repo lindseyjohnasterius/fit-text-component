6c92e3dd139ad304da17f80a01375e12886bd454
LNSY <lindsey.asterius@gmail.com>
2023-01-17 10:40:01 -0800

    Changed order for readability

--- fit-text-component.js
+++ fit-text-component.js
@@ -1,22 +1,17 @@
-
-
-
-
 class FitTextComponent extends HTMLElement {
   connectedCallback(){
     const text = this.innerHTML; 
     this.style.display = 'block';
     this.style.width = `100vw`;
     this.style.lineHeight = 1;
+    this.style.textAlign = `center`;
     let compressor = this.getAttribute('compressor');
     if(compressor === null){
       compressor = 1;
     }
     const text_size = (240 * compressor) / text.length;
     this.style.fontSize = `${text_size}vw`;
-    this.style.textAlign = `center`;
   }
-
 }
 
 customElements.define('fit-text', FitTextComponent)
